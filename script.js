08fd31e9c08473198476691f3bd251ca392796cf
emillyvitoriia <emilly.baldave.cruz@escola.pr.gov.br>
2025-10-27 13:35:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorAll(' .btn-proximo');
+
+avanca.forEach(buttton =>  {
+  button.addEventListener('click', function()  {
+     const atual = document.querySelector( ' .ativo');
+     const proximoPasso = 'paso1-' + this.getAttribute('data-proximo');
+
+  })
+
+
+})
